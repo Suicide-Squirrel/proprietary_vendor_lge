c06c27e6725ef7768efd7b1b37557024dae27f5c
kessaras <theodoroupaulos@gmail.com>
2017-11-27 14:14:09 +0200

    Add & Update more blobs to v29A

--- g4-common/Android.mk
+++ g4-common/Android.mk
@@ -124,6 +124,18 @@ LOCAL_PROPRIETARY_MODULE := true
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE := libqmi_client_helper
+LOCAL_MODULE_OWNER := lge
+LOCAL_SRC_FILES_64 := proprietary/vendor/lib64/libqmi_client_helper.so
+LOCAL_SRC_FILES_32 := proprietary/vendor/lib/libqmi_client_helper.so
+LOCAL_MULTILIB := both
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX := .so
+LOCAL_PROPRIETARY_MODULE := true
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE := libthermalclient
 LOCAL_MODULE_OWNER := lge
 LOCAL_SRC_FILES_64 := proprietary/vendor/lib64/libthermalclient.so
